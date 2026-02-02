40539b756c2f2187c9568037bd126f6be3529d3e
Wizzfi1 <shaibuwisdom@gmail.com>
2026-02-02 13:06:28 +0100

    Deployment to Render

--- src/db.js
+++ src/db.js
@@ -1,15 +1,19 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("connect", () => {
   console.log("Connected to Postgres");
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected PG error", err);
+  process.exit(1);
 });
 
 module.exports = pool;
